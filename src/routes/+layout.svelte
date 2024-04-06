b3ce4eb0d513f4381a864334339a2a3357422b44
Julian Espitaleta <guachamalinga@gmail.com>
2024-04-06 14:52:11 -0500

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,92 @@
+<script>
+	import './styles.css';
+	import { AppShell, AppRail, AppRailAnchor, AppRailTile, AppBar } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+
+	let menuActive = false;
+	let currentTile = 0;
+	let icon = 'mdi:hamburger';
+
+	const handleMenuChange = () => {
+		menuActive = !menuActive;
+		if (menuActive) {
+			icon = 'mdi:close';
+		} else {
+			icon = 'mdi:hamburger';
+		}
+	};
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<button on:click={handleMenuChange}>
+					<Icon {icon} width="2em" height="2em" class="transition-all ease-in-out" />
+				</button>
+			</svelte:fragment>
+			<p class="text-2xl">LoL Bingo</p>
+			<svelte:fragment slot="trail">(actions)</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<div
+			class="fixed z-20 transition-all duration-500 transform -translate-x-full"
+			class:lg:block={menuActive}
+			class:translate-x-0={menuActive}
+			class:shadow-2xl={menuActive}
+		>
+			<AppRail>
+				<!-- --- -->
+				<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+					<svelte:fragment slot="lead">
+						<div class="flex justify-center">
+							<Icon icon="mdi:home-outline" width="1.2em" height="1.2em" />
+						</div>
+					</svelte:fragment>
+					<span>Home</span>
+				</AppRailTile>
+				<AppRailTile bind:group={currentTile} name="tile-1" value={1} title="tile-1">
+					<svelte:fragment slot="lead">
+						<div class="flex justify-center">
+							<Icon icon="mdi:gamepad-square-outline" width="1.2em" height="1.2em" />
+						</div>
+					</svelte:fragment>
+					<span>Play Bingo</span>
+				</AppRailTile>
+				<AppRailTile bind:group={currentTile} name="tile-1" value={1} title="tile-1">
+					<svelte:fragment slot="lead">
+						<div class="flex justify-center">
+							<Icon icon="mdi:question-box-outline" width="1.2em" height="1.2em" />
+						</div>
+					</svelte:fragment>
+					<span>About</span>
+				</AppRailTile>
+				<!-- --- -->
+				<svelte:fragment slot="trail">
+					<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+				</svelte:fragment>
+			</AppRail>
+		</div>
+	</svelte:fragment>
+	<div class="p-4">
+		<slot />
+	</div>
+	<svelte:fragment slot="pageFooter">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<p>
+					Made with ❤️ by <a href="https://www.github.com/Guacha" target="_blank">Guacha</a>
+				</p>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a href="https://twitter.com/Guachamalinga" class="btn sm icon variant-filled">
+					<Icon icon="bi:twitter-x" width="0.8em" />
+				</a>
+				<a class="btn sm icon variant-filled-primary" href="https://www.github.com/Guacha">
+					<Icon icon="mdi:github" />
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8">
+	<div class="text-center space-y-4">
+		<h1 class="text-gray-800 font-bold text-4xl md:text-5xl">
+			Bored of simple League Games?
+			<span class="text-primary-500">Try the bingo challenge</span>
+		</h1>
+		<p class="text-gray-500 max-w-xl mx-auto leading-relaxed">
+			It is a long established fact that a reader will be distracted by the readable content of a
+			page when looking at its layout. The point of using Lorem Ipsum
+		</p>
+	</div>
+	<div class="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+		<a
+			href="/"
+			class="px-10 py-3.5 w-full bg-primary-600 text-white text-center rounded-md shadow-md block sm:w-auto hover:bg-primary-700 transition-all duration-300"
+		>
+			Start playing!
+		</a>
+		<a
+			href="/"
+			class="px-10 py-3.5 w-full text-gray-500 text-center border rounded-md duration-300 hover:text-secondary-600 hover:shadow block sm:w-auto"
+		>
+			Try it out
+		</a>
+	</div>
+</section>
